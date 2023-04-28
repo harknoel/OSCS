19ac92efc937b9e072fda74b40b69a17c56482c5
harknoel <bontilao.harknoel@gmail.com>
2023-04-28 23:56:29 +0800

    Add table in dashboard

--- dashboard.php
+++ dashboard.php
@@ -3,159 +3,36 @@
     require_once 'includes/header.php'; 
 ?>
 <!-- CONTENT -->
-<section id="content">
-	<!-- NAVBAR -->
-	<nav>
-		<i class='bx bx-menu' ></i>
-		<a href="#" class="nav-link">Categories</a>
-		<form action="#">
-			<div class="form-input">
-				<input type="search" placeholder="Search...">
-				<button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
-			</div>
-		</form>
-		<input type="checkbox" id="switch-mode" hidden>
-		<label for="switch-mode" class="switch-mode"></label>
-	</nav>
-	<!-- NAVBAR -->
-
-	<!-- MAIN -->
-	<main>
-		<div class="head-title">
-			<div class="left">
-				<h1>Dashboard</h1>
-				<ul class="breadcrumb">
-					<li>
-						<a href="#">Dashboard</a>
-					</li>
-					<li><i class='bx bx-chevron-right' ></i></li>
-					<li>
-						<a class="active" href="#">Home</a>
-					</li>
-				</ul>
-			</div>
-			<a href="#" class="btn-download">
-				<i class='bx bxs-cloud-download' ></i>
-				<span class="text">Download PDF</span>
-			</a>
-		</div>
-
-		<ul class="box-info">
-			<li>
-				<i class='bx bxs-calendar-check' ></i>
-				<span class="text">
-					<h3>1020</h3>
-					<p>New Order</p>
-				</span>
-			</li>
-			<li>
-				<i class='bx bxs-group' ></i>
-				<span class="text">
-					<h3>2834</h3>
-					<p>Visitors</p>
-				</span>
-			</li>
-			<li>
-				<i class='bx bxs-dollar-circle' ></i>
-				<span class="text">
-					<h3>$2543</h3>
-					<p>Total Sales</p>
-				</span>
-			</li>
-		</ul>
-
-
-		<div class="table-data">
-			<div class="order">
-				<div class="head">
-					<h3>Recent Orders</h3>
-					<i class='bx bx-search' ></i>
-					<i class='bx bx-filter' ></i>
-				</div>
-				<table>
-					<thead>
-						<tr>
-							<th>User</th>
-							<th>Date Order</th>
-							<th>Status</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>
-								<img src="img/people.png">
-								<p>John Doe</p>
-							</td>
-							<td>01-10-2021</td>
-							<td><span class="status completed">Completed</span></td>
-						</tr>
-						<tr>
-							<td>
-								<img src="img/people.png">
-								<p>John Doe</p>
-							</td>
-							<td>01-10-2021</td>
-							<td><span class="status pending">Pending</span></td>
-						</tr>
-						<tr>
-							<td>
-								<img src="img/people.png">
-								<p>John Doe</p>
-							</td>
-							<td>01-10-2021</td>
-							<td><span class="status process">Process</span></td>
-						</tr>
-						<tr>
-							<td>
-								<img src="img/people.png">
-								<p>John Doe</p>
-							</td>
-							<td>01-10-2021</td>
-							<td><span class="status pending">Pending</span></td>
-						</tr>
-						<tr>
-							<td>
-								<img src="img/people.png">
-								<p>John Doe</p>
-							</td>
-							<td>01-10-2021</td>
-							<td><span class="status completed">Completed</span></td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-			<div class="todo">
-				<div class="head">
-					<h3>Todos</h3>
-					<i class='bx bx-plus' ></i>
-					<i class='bx bx-filter' ></i>
-				</div>
-				<ul class="todo-list">
-					<li class="completed">
-						<p>Todo List</p>
-						<i class='bx bx-dots-vertical-rounded' ></i>
-					</li>
-					<li class="completed">
-						<p>Todo List</p>
-						<i class='bx bx-dots-vertical-rounded' ></i>
-					</li>
-					<li class="not-completed">
-						<p>Todo List</p>
-						<i class='bx bx-dots-vertical-rounded' ></i>
-					</li>
-					<li class="completed">
-						<p>Todo List</p>
-						<i class='bx bx-dots-vertical-rounded' ></i>
-					</li>
-					<li class="not-completed">
-						<p>Todo List</p>
-						<i class='bx bx-dots-vertical-rounded' ></i>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</main>
-	<!-- MAIN -->
-</section>
+<div> 
+	<?php
+		$mysqli = new mysqli('localhost', 'root','','dbf2bontilaohark') or die (mysqli_error($mysqli));
+		$resultset = $mysqli->query("SELECT * from tblGuest") or die($mysqli->error);
+	?>
+	<table id="tblStudentRecords " class="table
+		table-striped table-bordered table-sm" cellspacing="0" width="100%"> 
+		<thead>
+			<tr> 
+				<th>Guest ID</th> 
+				<th>Guest Name</th> 
+				<th>Country</th> 
+			</tr> 
+		</thead>  
+		<tbody>
+			<?php
+				while($row = $resultset->fetch_assoc()):
+			?>
+			<tr>
+				<td><?php echo $row['guestID'] ?></td>
+				<td><?php echo $row['guestName'] ?></td>
+				<td><?php echo $row['country'] ?></td>
+				<td>
+					<a href = "">VIEW</a>                        
+					<a href = "">DELETE</a>
+				</td>
+			</tr>
+			<?php endwhile;?>
+		</tbody>         
+	</table>
+</div>
 <!-- CONTENT -->
 <?php require_once 'includes/footer.php'; ?>
