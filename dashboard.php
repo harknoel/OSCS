bb43e471c46cff62830f5beef16330d64ccc58e8
harknoel <bontilao.harknoel@gmail.com>
2023-04-29 00:10:52 +0800

    Add table format in main

--- dashboard.php
+++ dashboard.php
@@ -3,36 +3,72 @@
     require_once 'includes/header.php'; 
 ?>
 <!-- CONTENT -->
-<div> 
-	<?php
-		$mysqli = new mysqli('localhost', 'root','','dbf2bontilaohark') or die (mysqli_error($mysqli));
-		$resultset = $mysqli->query("SELECT * from tblGuest") or die($mysqli->error);
-	?>
-	<table id="tblStudentRecords " class="table
-		table-striped table-bordered table-sm" cellspacing="0" width="100%"> 
-		<thead>
-			<tr> 
-				<th>Guest ID</th> 
-				<th>Guest Name</th> 
-				<th>Country</th> 
-			</tr> 
-		</thead>  
-		<tbody>
-			<?php
-				while($row = $resultset->fetch_assoc()):
-			?>
-			<tr>
-				<td><?php echo $row['guestID'] ?></td>
-				<td><?php echo $row['guestName'] ?></td>
-				<td><?php echo $row['country'] ?></td>
-				<td>
-					<a href = "">VIEW</a>                        
-					<a href = "">DELETE</a>
-				</td>
-			</tr>
-			<?php endwhile;?>
-		</tbody>         
-	</table>
-</div>
+		<!-- MAIN -->
+		
+			<div class="table-data">
+				<div class="order">
+					<div class="head">
+						<h3>Guest List</h3>
+						<i class='bx bx-search' ></i>
+						<i class='bx bx-filter' ></i>
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>User</th>
+								<th>Date Order</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status completed">Completed</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status pending">Pending</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status process">Process</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status pending">Pending</span></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>01-10-2021</td>
+								<td><span class="status completed">Completed</span></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+
+		</main>
+		<!-- MAIN -->
+	</section>
+	<!-- CONTENT -->
 <!-- CONTENT -->
 <?php require_once 'includes/footer.php'; ?>
--- includes/header.php
+++ includes/header.php
@@ -68,3 +68,24 @@
 		</ul>
 	</section>
 	<!-- SIDEBAR -->
+
+
+
+	<!-- CONTENT -->
+	<section id="content">
+	<!-- NAVBAR -->
+	<nav>
+		<i class='bx bx-menu' ></i>
+		<a href="#" class="nav-link">Categories</a>
+		<form action="#">
+			<div class="form-input">
+				<input type="search" placeholder="Search...">
+				<button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
+			</div>
+		</form>
+		<input type="checkbox" id="switch-mode" hidden>
+		<label for="switch-mode" class="switch-mode"></label>
+	</nav>
+	<!-- NAVBAR -->
+
+	<main>
