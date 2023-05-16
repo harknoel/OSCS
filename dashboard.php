a9f23896fa465737e0efa14b2e9721b72f75de63
harknoel <bontilao.harknoel@gmail.com>
2023-05-16 21:40:26 +0800

    Add total in the dashboard

--- dashboard.php
+++ dashboard.php
@@ -9,6 +9,38 @@
 	include 'session.php';
 	include 'login-check.php';
 ?>	
+  <h1 style="text-align: center; font-size: 32px; margin-top: 40px; margin-bottom: 20px;">Guest Users Dashboard</h1>
+  
+  <div style="display: flex; justify-content: space-around; margin-top: 20px;">
+    <div style="background-color: #f2f2f2; padding: 20px; border-radius: 10px; width: 300px; text-align: center;">
+      <h2>Number of Guest Users by Country</h2>
+      <p id="guestByCountry">Loading...</p>
+    </div>
+    
+    <div style="background-color: #f2f2f2; padding: 20px; border-radius: 10px; width: 300px; text-align: center;">
+      <h2>Number of Guest Users by Name</h2>
+      <p id="guestByName">Loading...</p>
+    </div>
+    
+    <div style="background-color: #f2f2f2; padding: 20px; border-radius: 10px; width: 300px; text-align: center;">
+      <h2>Total Number of Guest Users</h2>
+      <p id="guestOverall">Loading...</p>
+    </div>
+  </div>
+
+  <!-- Dummy JavaScript code to update the dashboard -->
+  <script>
+    // Replace the dummy values with actual data
+    const guestByCountry = 500;
+    const guestByName = 750;
+    const guestOverall = 1250;
+    
+    // Update the dashboard with the data
+    document.getElementById("guestByCountry").textContent = guestByCountry.toLocaleString();
+    document.getElementById("guestByName").textContent = guestByName.toLocaleString();
+    document.getElementById("guestOverall").textContent = guestOverall.toLocaleString();
+  </script>
+
 <?php
 	$query = 'SELECT * FROM tblguest';
 	$resultset = mysqli_query($con, $query);	
