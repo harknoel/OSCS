7ce02445a8a86159aba8b3d6dfafe5cc5f318d1c
harknoel <bontilao.harknoel@gmail.com>
2023-05-16 23:49:02 +0800

    initial dashboard

--- dashboard.php
+++ dashboard.php
@@ -69,77 +69,77 @@
 				const myChart = new Chart(document.getElementById('myChart'), config);
 			</script>
 		</div>
-		<div style="background-color: #f2f2f2; padding: 20px; border-radius: 10px; width: 300px; text-align: center;">
-			<div style="width: 600px; height: 400px; border: 1px solid #ccc; margin: 20px auto; padding: 10px;">
-				<h2 style="text-align: center;">Guest Users by Country over Time</h2>
-				<canvas id="lineChart" style="width: 100%; height: 100%;"></canvas>
-			</div>
+		<div style="background-color: #f2f2f2; padding: 20px; border-radius: 10px; width: 600px; text-align: center;">
+  <div style="width: 100%; height: 400px; border: 1px solid #ccc; margin: 20px auto; padding: 10px;">
+    <h2 style="text-align: center;">Guest Users by Country over Time</h2>
+    <canvas id="lineChart" style="width: 100%; height: 100%;"></canvas>
+  </div>
 
-			<script>
-				// Sample Data for Line Chart
-				const lineData = {
-				labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-				datasets: [
-					{
-					label: 'United States',
-					data: [50, 75, 80, 60, 70, 90],
-					borderColor: 'rgba(75, 192, 192, 1)',
-					fill: false
-					},
-					{
-					label: 'United Kingdom',
-					data: [60, 55, 70, 80, 65, 75],
-					borderColor: 'rgba(255, 99, 132, 1)',
-					fill: false
-					},
-					{
-					label: 'Germany',
-					data: [70, 80, 60, 70, 85, 65],
-					borderColor: 'rgba(54, 162, 235, 1)',
-					fill: false
-					}
-				]
-				};
+  <script>
+    // Sample Data for Line Chart
+    const lineData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [
+        {
+          label: 'United States',
+          data: [50, 75, 80, 60, 70, 90],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          fill: false
+        },
+        {
+          label: 'United Kingdom',
+          data: [60, 55, 70, 80, 65, 75],
+          borderColor: 'rgba(255, 99, 132, 1)',
+          fill: false
+        },
+        {
+          label: 'Germany',
+          data: [70, 80, 60, 70, 85, 65],
+          borderColor: 'rgba(54, 162, 235, 1)',
+          fill: false
+        }
+      ]
+    };
 
-				// Line Chart Configuration
-				const lineConfig = {
-				type: 'line',
-				data: lineData,
-				options: {
-					responsive: true,
-					maintainAspectRatio: false,
-					plugins: {
-					title: {
-						display: true,
-						text: 'Guest Users by Country over Time'
-					}
-					},
-					scales: {
-					x: {
-						display: true,
-						title: {
-						display: true,
-						text: 'Month'
-						}
-					},
-					y: {
-						display: true,
-						title: {
-						display: true,
-						text: 'Number of Guest Users'
-						},
-						suggestedMin: 0,
-						suggestedMax: 100
-					}
-					}
-				}
-				};
+    // Line Chart Configuration
+    const lineConfig = {
+      type: 'line',
+      data: lineData,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Guest Users by Country over Time'
+          }
+        },
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Month'
+            }
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Number of Guest Users'
+            },
+            suggestedMin: 0,
+            suggestedMax: 100
+          }
+        }
+      }
+    };
 
-				// Create Line Chart
-				const lineCtx = document.getElementById('lineChart').getContext('2d');
-				new Chart(lineCtx, lineConfig);
-			</script>
-		</div>
+    // Create Line Chart
+    const lineCtx = document.getElementById('lineChart').getContext('2d');
+    new Chart(lineCtx, lineConfig);
+  </script>
+</div>
 	</div>
 	<!-- Dummy JavaScript code to update the dashboard -->
 	<script>
